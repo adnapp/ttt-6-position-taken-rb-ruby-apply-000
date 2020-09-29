52f9ab12f5177575ee5d9a47f4b0a2272a73e2b0
Adam Napoletano <adam.napps@gmail.com>
2020-09-29 02:05:36 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
-def position_taken?(index_number)
+def position_taken?(array, index_number)
 if array[index_number] == "" || array[index_number] == " "
   false
 else
